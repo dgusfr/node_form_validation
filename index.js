8d0432a9ac353bcdf9f0df113016c5c059dfda5f
dgusfr <diegogustavo.fr@gmail.com>
2024-06-18 09:35:59 -0300

    changes

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var session = require("express-session");
 var flash = require("connect-flash");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.render("index.ejs");
-});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -20,6 +16,19 @@ app.use(
   })
 );
 app.use(flash());
+
+app.get("/", function (req, res) {
+  res.render("index.ejs");
+});
+
+app.post("/form", (req, res) => {
+  var { email, name, password } = req.body;
+  var emailError;
+  if (email == undefined || email == "") {
+    emailError = "Please fill all the fields";
+  }
+  res.send(email);
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
