3b3650c938a1dcbf9d3c26dd1d66337c872aeff4
dgusfr <diegogustavo.fr@gmail.com>
2024-06-18 09:18:54 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+npm install express --save
+
+npm install body-parser --save
+
+npm install express-session --save
+
+npm install ejs --save
+
+npm install express-flash --save
---
+++ index.js
@@ -0,0 +1,26 @@
+const bodyParser = require("body-parser");
+var express = require("express");
+var session = require("express-session");
+var flash = require("connect-flash");
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+app.use(flash());
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
